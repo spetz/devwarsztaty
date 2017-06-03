b72d86de6746145836348885418199c004c34d11
Piotr Gankiewicz <spetz90@gmail.com>
2017-06-03 10:39:25 +0200

    Event handler, records controller

---
+++ Warsztaty/Warsztaty/Controllers/RecordsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using warsztaty.messages.Commands;
+
+namespace Warsztaty.API.Controllers
+{
+    [Route("[controller]")]
+    public class RecordsController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreateRecord command)
+        {
+            return Accepted();
+        }
+    }
+}
---
+++ Warsztaty/warsztaty.messages/Events/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace warsztaty.messages.Events
+{
+    public interface IEventHandler<in T> where T : IEvent
+	{
+        Task HandleAsync(T @event);
+	}
+}
